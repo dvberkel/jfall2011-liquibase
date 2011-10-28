166d801dd143b392541c8236e5a357afdd81d6ab
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-10-28 14:25:09 +0200

    Library Introduced
    The JFall::Util package is introduced.

---
+++ JFall/Util.pm
@@ -0,0 +1,51 @@
+package JFall::Util;
+
+use strict;
+use warnings;
+
+require Exporter;
+
+our $VERSION = '1.00';
+
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(checkout nextBranch currentBranch);
+
+sub checkout {
+	my $branch = shift @_;
+	system("git checkout $branch");
+}
+
+sub nextBranch {
+	my $nextBranch = shift @_;
+	if ($nextBranch =~ m/(.+)(-)(\d+)/) {
+		my $branchName = $1;
+		my $separator = $2;
+		my $stage = $3;
+		$nextBranch = $branchName . $separator . nextStage($stage);
+	}
+	return $nextBranch;
+}
+
+sub nextStage {
+	my $stage = shift @_;
+	my $nextStage = scalar($stage) + 1;
+	
+	if ($nextStage < 10) {
+		return "0$nextStage";
+	} else {
+		return "$nextStage";
+	}
+}
+
+sub currentBranch {
+	my $currentBranch = '';
+	my @branches = split "\n", `git branch`;
+	while (my $branch = shift @branches) {
+		if ($branch =~ m/^\*\s+(\S+)/) {
+			$currentBranch = $1;
+			last;
+		}
+	}
+	
+	return $currentBranch;
+}
