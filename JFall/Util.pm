2a87ce9b5e4e884f872f01520244def4ae21a65d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-10-28 14:29:15 +0200

    Down introduced
    The inverse command to up is introduced.

--- JFall/Util.pm
+++ JFall/Util.pm
@@ -8,7 +8,7 @@ require Exporter;
 our $VERSION = '1.00';
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(checkout nextBranch currentBranch);
+our @EXPORT_OK = qw(checkout nextBranch previousBranch currentBranch);
 
 sub checkout {
 	my $branch = shift @_;
@@ -29,6 +29,28 @@ sub nextBranch {
 sub nextStage {
 	my $stage = shift @_;
 	my $nextStage = scalar($stage) + 1;
+	
+	if ($nextStage < 10) {
+		return "0$nextStage";
+	} else {
+		return "$nextStage";
+	}
+}
+
+sub previousBranch {
+	my $nextBranch = shift @_;
+	if ($nextBranch =~ m/(.+)(-)(\d+)/) {
+		my $branchName = $1;
+		my $separator = $2;
+		my $stage = $3;
+		$nextBranch = $branchName . $separator . previousStage($stage);
+	}
+	return $nextBranch;
+}
+
+sub previousStage {
+	my $stage = shift @_;
+	my $nextStage = scalar($stage) - 1;
 	
 	if ($nextStage < 10) {
 		return "0$nextStage";
